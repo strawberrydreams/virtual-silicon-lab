8eda699bac09140a2b9e12e41b1d0e7645b98a0a
strawberrydreams <jh08037@naver.com>
2026-06-03 11:15:41 +0900

    feat: render a dedicated die only export stage

---
+++ src/features/export/DieExportStage.tsx
@@ -0,0 +1,17 @@
+import { forwardRef } from 'react'
+import type Konva from 'konva'
+import { Layer, Stage } from 'react-konva'
+import type { Project } from '../../domain/project'
+import { ChipArtwork } from '../editor/canvas/ChipArtwork'
+
+export const DieExportStage = forwardRef<Konva.Stage, { project: Project }>(
+  function DieExportStage({ project }, ref) {
+    return (
+      <Stage ref={ref} width={project.die.width} height={project.die.height}>
+        <Layer>
+          <ChipArtwork project={project} />
+        </Layer>
+      </Stage>
+    )
+  },
+)
